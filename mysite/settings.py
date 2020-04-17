00e66df99bd166a3e686df8f20c958141022f453
tettsu9657 <63411453+tettsu9657@users.noreply.github.com>
2020-04-18 00:07:36 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z3e@-(75)x)lmv@qu$wpj)juraj7$&krw3#_5sdwh%m#_$fb@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com','tettsu.pythonanywhere.com']
 
